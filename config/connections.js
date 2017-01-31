adca19bce0a9f362fa7dae90467700465741ae64
Saad <Saad>
2017-01-31 22:44:59 +0500

    Configure for mongolab.

--- config/connections.js
+++ config/connections.js
@@ -58,8 +58,9 @@ module.exports.connections = {
   ***************************************************************************/
   localMongoDb: {
      adapter: 'sails-mongo',
-     host: 'localhost',
-     port: 27017,
+     url: process.env.MONGODB_URI,
+     //host: 'localhost',
+     //port: 27017,
   //   user: 'username', //optional
   //   password: 'password', //optional
   //   database: 'your_mongo_db_name_here' //optional
--- config/models.js
+++ config/models.js
@@ -17,7 +17,8 @@ module.exports.models = {
   * connections (see `config/connections.js`)                                *
   *                                                                          *
   ***************************************************************************/
-  //  connection: 'localMongoDb',
+    connection: 'localMongoDb'
+    //connection: 'mongoLabMongoDb',
   //
   // /***************************************************************************
   // *                                                                          *
